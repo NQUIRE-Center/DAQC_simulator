fb07030bbb107c3f97f022685011813fc714d228
MikelGdeA <96070190+MikelGdeA@users.noreply.github.com>
2023-11-21 10:21:24 +0100

    Update D_simulator.m
    Copyright update

--- D_simulator.m
+++ D_simulator.m
@@ -1,3 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% By: Mikel Garcia de Andoin, mikelgda@gmail.com
+% Licensed under Licensed under CC BY 4.0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [rho,rhomeas,Uideal] = D_simulator(gate_type,angles,CNOT_ctrl,CNOT_targ,measqubits,rxgatetime,rzgatetime,CNOTgatetime,CNOTerror,rD,pbf,pmeas,pth,T1,T2,rho)
 %DA_SIMULATOR Function to simulate the application of a digital circuit in a noisy platform.
 %   gate_type               Type of SQG that is applied.
